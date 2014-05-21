7d43d9e2405134def9387b4f477274c632983758
fmahon <florian.mahon@gmail.com>
2014-05-21 13:21:54 +0200

    Read directory configuration

--- src/main/java/org/fetm/backuptools/command/Main.java
+++ src/main/java/org/fetm/backuptools/command/Main.java
@@ -1,21 +1,19 @@
 package org.fetm.backuptools.command;
 
-import org.fetm.backuptools.common.BackupAgent;
-import org.fetm.backuptools.common.BackupAgentFactory;
-import org.fetm.backuptools.common.datanode.*;
-import org.fetm.backuptools.common.tools.ScpClient;
 import org.apache.commons.cli.*;
+import org.fetm.backuptools.common.BackupAgentFactory;
+import org.fetm.backuptools.common.IBackupAgent;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
 
-/******************************************************************************
+/*****************************************************************************
  * Copyright (c) 2013,2014. Florian Mahon <florian@faivre-et-mahon.ch>        *
  *                                                                            *
  * This file is part of backuptools.                                          *
@@ -31,29 +29,36 @@ import java.util.Properties;
  * General Public License for more details. You should have received a        *
  * copy of the GNU General Public License along with this program.            *
  * If not, see <http://www.gnu.org/licenses/>.                                *
- ******************************************************************************/
+ * ***************************************************************************/
 
 public class Main {
+
+    public static final String OPT_HELP = "h";
+    public static final String OPT_FILE = "f";
+    public static final String OPT_INIT = "i";
+    public static final String OPT_DIRECTORY = "d";
+
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         main.execute(args);
         System.exit(0);
     }
 
-    private void execute(String[] args) throws IOException  {
+    private void execute(String[] args) throws IOException {
 
         Options options = new Options();
-
-        options.addOption("h"    , false,"Display this help");
-        options.addOption("f"    , true ,"set configuration file");
-        options.addOption("i"    , true ,"initialise new file configuration");
+        List<IBackupAgent> agents = new ArrayList<>();
+        options.addOption(OPT_HELP, false, "Display this help");
+        options.addOption(OPT_FILE, true, "set configuration file");
+        options.addOption(OPT_INIT, true, "create new configuration file");
+        options.addOption(OPT_DIRECTORY, true,  "set directory contain multi configurations files '*.properties'");
 
         CommandLineParser parser = new BasicParser();
         CommandLine cmd = null;
 
         try {
             cmd = parser.parse(options, args);
-            if(cmd.getOptions().length==0){
+            if (cmd.getOptions().length == 0) {
                 throw new Exception("Require option!");
             }
         } catch (Exception e) {
@@ -62,23 +67,29 @@ public class Main {
             System.exit(-1);
         }
 
-
-        if(cmd.hasOption("i")){
-            String filename = cmd.getOptionValue("i");
+        if (cmd.hasOption(OPT_INIT)) {
+            String filename = cmd.getOptionValue(OPT_INIT);
             Path file = Paths.get(filename);
             initConfigFile(file);
-        }
-        else if(cmd.hasOption("h")){
+        } else if (cmd.hasOption(OPT_HELP)) {
             usage(options);
-        } else if(cmd.hasOption("f")){
-            String file = cmd.getOptionValue("f");
-            BackupAgent agent = BackupAgentFactory.getBackupAgent(file);
-            agent.doBackup();
-        } else if(cmd.hasOption("d")){
-            String directory = cmd.getOptionValue("d");
+        } else if (cmd.hasOption(OPT_FILE)) {
+            String file = cmd.getOptionValue(OPT_FILE);
+            agents.add(BackupAgentFactory.getBackupAgent(file));
+        } else if (cmd.hasOption(OPT_DIRECTORY)) {
+            String directory = cmd.getOptionValue(OPT_DIRECTORY);
+            Path path = Paths.get(directory);
+            if(path.toFile().isDirectory()){
+                agents.addAll(BackupAgentFactory.getBackupAgents(path));
+            }
         }
 
+        for(IBackupAgent agent : agents){
+            agent.doBackup();
+        }
     }
+
+
 
     private void initConfigFile(Path file) throws IOException {
         URL URLFile = getClass().getClassLoader().getResource("configuration.properties");
@@ -88,6 +99,6 @@ public class Main {
 
     private void usage(Options options) {
         HelpFormatter hp = new HelpFormatter();
-        hp.printHelp("backuptools",options);
+        hp.printHelp("backuptools", options);
     }
 }
